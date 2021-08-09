a197f8275601a9e12ade51ad2d709bdcd2f87534
Yaneizy Castillo <yccastillo18@gmail.com>
2021-08-09 13:37:19 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code in this file!
+//const currentUser = 'Grace Hopper';
+
+//const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+//const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+
+//const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+//////////////// Example from lesson ////////////////////
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
